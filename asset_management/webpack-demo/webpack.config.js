4941030090ee79795afb2a8b82f034f950519b4e
siddharthsachdeva2835 <siddharthsachdeva2835@gmail.com>
2019-03-19 00:17:28 +0530

    asset management module final

--- asset_management/webpack-demo/webpack.config.js
+++ asset_management/webpack-demo/webpack.config.js
@@ -27,7 +27,19 @@ module.exports = {
                 use: [
                     'file-loader'
                 ]
-           }
+           },
+           {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
         ]
     }
 }
